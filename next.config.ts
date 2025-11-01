8541b904a71447d83fefd9ac9e9c205017fa41dc
MR-MARCOSs <marcosdudu001@gmail.com>
2025-11-01 17:51:55 -0300

    fix: backend build

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'standalone',
+  experimental: {
+    outputFileTracingExcludes: {
+      '*': ['./backend/**/*'],
+    },
+  },
 };
 
 export default nextConfig;
